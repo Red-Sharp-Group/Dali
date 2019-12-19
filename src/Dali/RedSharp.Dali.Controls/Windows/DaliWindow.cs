d1e77c3b9987b9b6fd908cbeeedeae03d50e4f82
Влад Стовманенко <flaviusglamfenix@gmail.com>
2019-12-19 22:58:57 +0200

    Creating structure for RedSharp.Dali.Controls project. Updating project target architecture.

---
+++ src/Dali/RedSharp.Dali.Controls/Windows/DaliWindow.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace RedSharp.Dali.Controls.Windows
+{
+    public class DaliWindow : Window
+    {
+        static DaliWindow()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(DaliWindow), new FrameworkPropertyMetadata(typeof(DaliWindow)));
+        }
+
+        #region Depdendency properties
+
+        #region CanClose property
+        public static readonly DependencyProperty CanCloseProperty =
+                DependencyProperty.Register(nameof(CanClose), typeof(bool), typeof(DaliWindow), new PropertyMetadata(true));
+
+        public bool CanClose
+        {
+            get => (bool)GetValue(CanCloseProperty);
+            set => SetValue(CanCloseProperty, value);
+        }
+        #endregion
+
+        #region CanMaximize property
+        public static readonly DependencyProperty CanMaximizeProperty =
+                DependencyProperty.Register(nameof(CanMaximize), typeof(bool), typeof(DaliWindow), new PropertyMetadata(true));
+
+        public bool CanMaximize
+        {
+            get => (bool)GetValue(CanMaximizeProperty);
+            set => SetValue(CanMaximizeProperty, value);
+        }
+        #endregion
+
+        #region CanMinimize property
+        public static readonly DependencyProperty CanMinimizeProperty =
+                DependencyProperty.Register(nameof(CanMinimize), typeof(bool), typeof(DaliWindow), new PropertyMetadata(true));
+
+        public bool CanMinimize
+        {
+            get => (bool)GetValue(CanMinimizeProperty);
+            set => SetValue(CanMinimizeProperty, value);
+        }
+        #endregion
+
+        #endregion
+
+    }
+}

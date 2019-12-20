4cb27c6b724fdd569ae2cd484f7e7d368af0a9b7
Andrii Kudriavstev <43117099+KudriavtsevAndrii@users.noreply.github.com>
2019-12-20 15:47:15 +0200

    5 Global Hotkes pull request (#13)
    * Several native helpers were added to the common project, also HotkeyProvider was added.
    
    * New assembly for native interop was added to the solution. XML docs were added.
    
    * Mechanism of event invoking was changed.

---
+++ src/Dali/RedSharp.Dali.Common/Enums/HotkeyModifier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedSharp.Dali.Common.Enums
+{
+    /// <summary>
+    /// List of modify buttons for register global HotKeys.
+    /// </summary>
+    /// <SecurityNote>
+    /// Do not change values.
+    /// </SecurityNote>
+    public enum HotkeyModifier
+    {
+        None = 0,
+        Alt = 1,
+        Ctrl = 2,
+        Shift = 4,
+        Win = 8
+    }
+}
---
+++ src/Dali/RedSharp.Dali.Common/Interfaces/IControlledDisposable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedSharp.Dali.Common.Interfaces
+{
+    /// <summary>
+    /// Created for ability to find out already disposed objects.
+    /// </summary>
+    public interface IControlledDisposable : IDisposable
+    {
+        /// <summary>
+        /// Marks that current instance is released and can't be used anymore.
+        /// </summary>
+        /// <remarks>
+        /// Safe to invoke - all types, which will implement this 
+        /// must guarantee do not throw an exception when calling this property.
+        /// </remarks>
+        bool IsDisposed { get; }
+    }
+}

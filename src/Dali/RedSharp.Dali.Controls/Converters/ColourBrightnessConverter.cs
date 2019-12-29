804c0ccb8e1533eeab8de1aab3d2e6a491cd1124
Влад Стовманенко <flaviusglamfenix@gmail.com>
2019-12-29 17:44:15 +0200

    Adding style for buttons.

---
+++ src/Dali/RedSharp.Dali.Controls/Converters/ColourBrightnessConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace RedSharp.Dali.Controls.Converters
+{
+    class ColourBrightnessConverter : IValueConverter
+    {
+        private SolidColorBrush ModifyColour(Color colour, double coef)
+        {
+            byte R, G, B;
+
+            R = Math.Min((byte)255, (byte)(colour.R * coef));
+            G = Math.Min((byte)255, (byte)(colour.G * coef));
+            B = Math.Min((byte)255, (byte)(colour.B * coef));
+
+            return new SolidColorBrush(Color.FromArgb(colour.A, R, G, B));
+        }
+
+        private SolidColorBrush ModifyBrush(SolidColorBrush brush, double coef)
+        {
+            Color colour = brush.Color;
+            colour.A = (byte)(brush.Opacity * 255);
+
+            return ModifyColour(colour, coef);
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double coef;
+            if (!double.TryParse(parameter.ToString(), NumberStyles.Any, CultureInfo.InvariantCulture, out coef))
+                throw new Exception("Not a number");
+
+            if (value is Color colour)
+                return ModifyColour(colour, coef);
+            else if (value is SolidColorBrush brush)
+                return ModifyBrush(brush, coef);
+            else
+                throw new ArgumentException("Cannot work with such colour representation");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Dali/RedSharp.Dali.Controls/Converters/TextColorSelector.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace RedSharp.Dali.Controls.Converters
+{
+    class TextColorSelector : IValueConverter
+    {
+        private SolidColorBrush ModifyColour(Color c)
+        {
+            double brightness = Math.Sqrt(c.R * c.R * .241 + c.G * c.G * .691 + c.B * c.B * .068);
+
+            return brightness > 127 ? Brushes.Black : Brushes.White;
+        }
+
+        private SolidColorBrush ModifyBrush(SolidColorBrush brush)
+        {
+            Color colour = brush.Color;
+            colour.A = (byte)(brush.Opacity * 255);
+
+            return ModifyColour(colour);
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Color colour)
+                return ModifyColour(colour);
+            else if (value is SolidColorBrush brush)
+                return ModifyBrush(brush);
+            else
+                throw new ArgumentException("Cannot work with such colour representation");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

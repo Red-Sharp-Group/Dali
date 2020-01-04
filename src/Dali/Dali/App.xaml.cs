64c10443855f4bc2e836f3f303543cbb412c311a
Влад Стовманенко <flaviusglamfenix@gmail.com>
2020-01-04 15:31:26 +0200

    Adding MainWindowViewModel. Providing DI Container.

--- src/Dali/Dali/App.xaml.cs
+++ src/Dali/Dali/App.xaml.cs
@@ -1,14 +1,31 @@
-﻿using System;
+﻿using RedSharp.Dali.View;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Unity;
 
 namespace RedSharp.Dali
 {
     public partial class App : Application
     {
+        public IUnityContainer Container { get; private set; }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            InitializeContainer();
+
+            MainWindow = Container.Resolve<MainWindow>();
+            MainWindow.Show();
+        }
+
+        private void InitializeContainer()
+        {
+            Container = new UnityContainer();
+        }
     }
 }
---
+++ src/Dali/RedSharp.Dali.ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,51 @@
+﻿using ReactiveUI;
+using System;
+using System.Reactive;
+
+namespace RedSharp.Dali.ViewModel
+{
+    public class MainWindowViewModel : ReactiveObject
+    {
+        private ReactiveCommand<Unit, Unit> _startCommand;
+        private ReactiveCommand<Unit, Unit> _loadCommand;
+        private ReactiveCommand<Unit, Unit> _removeCommand;
+
+        public MainWindowViewModel()
+        {
+            
+        }
+
+        public ReactiveCommand<Unit, Unit> StartCommand
+        {
+            get
+            {
+                return _startCommand ?? ( _startCommand = ReactiveCommand.Create(() => 
+                {
+
+                }));
+            }
+        }
+
+        public ReactiveCommand<Unit, Unit> LoadCommand
+        {
+            get
+            {
+                return _loadCommand ?? (_loadCommand = ReactiveCommand.Create(() =>
+                {
+
+                }));
+            }
+        }
+
+        public ReactiveCommand<Unit, Unit> RemoveCommand
+        {
+            get
+            {
+                return _removeCommand ?? (_removeCommand = ReactiveCommand.Create(() =>
+                {
+
+                }));
+            }
+        }
+    }
+}

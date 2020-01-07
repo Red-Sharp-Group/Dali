9258b610b3119a263d9f0097cbf0cd15b57bc98e
Andrii Kudriavstev <43117099+KudriavtsevAndrii@users.noreply.github.com>
2020-01-07 17:24:28 +0200

    Feature/8 input transparency (#22)
    * Helper with ability to make window become input transparency was added.

---
+++ src/Dali/RedSharp.Dali.Common.Interop/Helpers/WindowsStyleHelper.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+using RedSharp.Dali.Common.Interop.Native.Enums;
+
+namespace RedSharp.Dali.Common.Interop.Helpers
+{
+    /// <summary>
+    /// Helps to set extended style for the WPF window in safe manner.
+    /// </summary>
+    public static class WindowsStyleHelper
+    {
+        private const String InputWindowHasInvalidHandle = "Input window has invalid handle.";
+
+        /// <summary>
+        /// Enable mouse input transparency for the given window.
+        /// It doesn't work if window isn't layered.
+        /// </summary>
+        /// <remarks>
+        /// Returns true if input transparency is already enabled.
+        /// Returns false if window isn't layered.
+        /// </remarks>
+        /// <exception cref="ArgumentNullException">
+        /// If input window is null.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// If input window handle is equal to IntPtr.Zero.
+        /// </exception>
+        /// <SecurityNote>
+        /// This method isn't security critical, because work with window handle going on internally.
+        /// </SecurityNote>
+        public static bool TryEnableInputTransparency(Window window)
+        {
+            if (window is null)
+                throw new ArgumentNullException(nameof(window));
+
+            IntPtr handle = InternalWindowsHelper.GetWindowHandle(window);
+
+            if (handle == IntPtr.Zero)
+                throw new ArgumentException(InputWindowHasInvalidHandle);
+
+            WindowStylesEx windowStyles = InternalWindowsHelper.GetWindowsExtendedStyle(handle);
+
+            if (!windowStyles.HasFlag(WindowStylesEx.WS_EX_LAYERED))
+                return false;
+
+            if (!windowStyles.HasFlag(WindowStylesEx.WS_EX_TRANSPARENT))
+            {
+                WindowStylesEx newStyle = windowStyles | WindowStylesEx.WS_EX_TRANSPARENT;
+
+                return InternalWindowsHelper.TrySetWindowsExtendedStyle(handle, newStyle);
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Disable mouse input transparency for the given window.
+        /// </summary>
+        /// <remarks>
+        /// Returns true if input transparency is already disabled or if window isn't layered.
+        /// </remarks>
+        /// <exception cref="ArgumentNullException">
+        /// If input window is null.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// If input window handle is equal to IntPtr.Zero.
+        /// </exception>
+        /// <SecurityNote>
+        /// This method isn't security critical, because work with window handle going on internally.
+        /// </SecurityNote>
+        public static bool TryDisableInputTransparency(Window window)
+        {
+            if (window is null)
+                throw new ArgumentNullException(nameof(window));
+
+            IntPtr handle = InternalWindowsHelper.GetWindowHandle(window);
+
+            if (handle == IntPtr.Zero)
+                throw new ArgumentException(InputWindowHasInvalidHandle);
+
+            WindowStylesEx windowStyles = InternalWindowsHelper.GetWindowsExtendedStyle(handle);
+
+            if (!windowStyles.HasFlag(WindowStylesEx.WS_EX_LAYERED))
+                return false;
+
+            if (windowStyles.HasFlag(WindowStylesEx.WS_EX_TRANSPARENT))
+            {
+                WindowStylesEx newStyle = windowStyles & ~WindowStylesEx.WS_EX_TRANSPARENT;
+
+                return InternalWindowsHelper.TrySetWindowsExtendedStyle(handle, newStyle);
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Checks that input window is transparency for the mouse input.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">
+        /// If input window is null.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// If input window handle is equal to IntPtr.Zero.
+        /// </exception>
+        /// <SecurityNote>
+        /// This method isn't security critical, because work with window handle going on internally.
+        /// </SecurityNote>
+        public static bool HasInputTransparency(Window window)
+        {
+            if (window is null)
+                throw new ArgumentNullException(nameof(window));
+
+            IntPtr handle = InternalWindowsHelper.GetWindowHandle(window);
+
+            if(handle == IntPtr.Zero)
+                throw new ArgumentException(InputWindowHasInvalidHandle);
+
+            return InternalWindowsHelper.IsContainExtendedStyle(handle, WindowStylesEx.WS_EX_TRANSPARENT);
+        }
+    }
+}
---
+++ src/Dali/RedSharp.Dali.Common.Interop/Native/Enums/WindowsLong.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedSharp.Dali.Common.Interop.Native.Enums
+{
+    /// <summary>
+    /// Used in the GetWindowLong and SetWindowLong functions as index value.
+    /// In the general idea this is the already predefined zero-based offset 
+    /// to the value to be retrieved from the windows structure. 
+    /// <para/>
+    /// See more: <see cref="https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getwindowlonga"/>
+    /// </summary>
+    internal enum WindowsLong : int
+    {
+        /// <summary>
+        /// Sets a new extended window style.
+        /// </summary>
+        GWL_EXSTYLE = -20,
+
+        /// <summary>
+        /// Sets a new application instance handle.
+        /// </summary>
+        GWL_HINSTANCE = -6,
+
+        /// <summary>
+        /// Sets a new identifier of the child window.
+        /// The window cannot be a top-level window.
+        /// </summary>
+        GWL_ID = -12,
+
+        /// <summary>
+        /// Sets a new window style.
+        /// </summary>
+        GWL_STYLE = -16,
+
+        /// <summary>
+        /// Sets the user data associated with the window. 
+        /// This data is intended for use by the application that created the window. 
+        /// Its value is initially zero.
+        /// </summary>
+        GWL_USERDATA = -21,
+
+        /// <summary>
+        /// Sets a new address for the window procedure.
+        /// You cannot change this attribute if the window does not belong 
+        /// to the same process as the calling thread.
+        /// </summary>
+        GWL_WNDPROC = -4,
+    }
+}

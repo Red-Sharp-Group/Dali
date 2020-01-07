2d318b6b5785deb5a9b0135ba05799c5cd7b2f34
Andrii Kudriavtsev <extosis.vt@gmail.com>
2020-01-07 16:42:15 +0200

    NUnit tests project was added and configure.

---
+++ src/Dali/RedSharp.Dali.NUnitTests/Common/Interfaces/IControlledDisposableTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using NUnit;
+using NUnit.Framework;
+
+using RedSharp.Dali.Common.Interop;
+
+namespace RedSharp.Dali.NUnitTests.Common.Interfaces
+{
+    [TestFixture]
+    public class IControlledDisposableTests
+    {
+        public static Func<IDisposable>[] TestCases = new Func<IDisposable>[]
+        {
+
+        };
+
+        public IDisposable _testedValue;
+
+        [TearDown]
+        public void AfterTest()
+        {
+            if(_testedValue != null)
+            {
+                try
+                {
+                    _testedValue.Dispose();
+                }
+                catch(Exception exception)
+                {
+                    Trace.WriteLine(exception.Message);
+                    Trace.WriteLine(exception.StackTrace);
+                }
+            }
+        }
+    }
+}

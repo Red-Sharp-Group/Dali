96b9b9ea92c8e0707e9bc188205b6fa2d616d575
Влад Стовманенко <flaviusglamfenix@gmail.com>
2020-01-17 07:42:58 +0200

    Adding class for Image Storing.

---
+++ src/Dali/RedSharp.Dali.ViewModel/ImageItem.cs
@@ -0,0 +1,34 @@
+﻿using ReactiveUI;
+using SixLabors.ImageSharp;
+using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedSharp.Dali.ViewModel
+{
+    class ImageItem : ReactiveObject
+    {
+        public static bool CacheImages { get; set; }
+
+        private Image _image;
+        private string _path;
+        private byte[] _cache;
+        private Image _preview;
+        public ImageItem(string path)
+        { }
+
+        public ImageItem(byte[] rawBites)
+        { }
+
+        /*public ImageItem(Memory<byte> memory)
+        {
+            MemoryHandle mem = memory.Pin();
+
+            unsafe
+            {
+                Image.Load(new ReadOnlySpan<byte>(mem.Pointer, memory.Length));
+            }
+        }*/
+    }
+}

8b149544dfa3c8df96a2d7141cbdb10105a49d74
Влад Стовманенко <flaviusglamfenix@gmail.com>
2020-01-27 15:06:16 +0200

    Add platform independent Drag and drop event args and required subtypes. Adding trigger action to call VM commands.

---
+++ src/Dali/Dali/Actions/ConvertDragAndDropArgsAction.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Xaml.Behaviors;
+using RedSharp.Dali.Common.Enums;
+using RedSharp.Dali.Common.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace RedSharp.Dali.Actions
+{
+    internal class ConvertDragAndDropArgsAction : TriggerAction<ListBox>
+    {
+        public static readonly DependencyProperty CommandProperty =
+                   DependencyProperty.Register(nameof(Command), typeof(ICommand), typeof(ConvertDragAndDropArgsAction));
+
+        public ICommand Command
+        {
+            get => GetValue(CommandProperty) as ICommand;
+            set => SetValue(CommandProperty, value);
+        }
+
+        protected override void Invoke(object parameter)
+        {
+            if (this.AssociatedObject != null && Command != null)
+            {
+                DragEventArgs eventArgs = parameter as DragEventArgs;
+
+                DragAndDropEventArgs daliArgs = new DragAndDropEventArgs()
+                {
+                    AllowedEffects = ToDaliDragAndDropEffectsEnum(eventArgs.AllowedEffects),
+                    Data = ToDragDataDictionary(eventArgs.Data),
+                    Effects = ToDaliDragAndDropEffectsEnum(eventArgs.Effects),
+                    Handled = eventArgs.Handled,
+                    KeyStates = ToDaliDragAndDropKeyStatesEnum(eventArgs.KeyStates)
+                };
+
+                if (Command.CanExecute(daliArgs))
+                {
+                    Command.Execute(daliArgs);
+                }
+
+                eventArgs.Effects = ToWpfDragDropEffects(daliArgs.Effects);
+                eventArgs.Handled = daliArgs.Handled;
+            }
+            else
+            {
+                //ToDo:
+                //Add exception or trace message here.
+            }
+        }
+
+        private DragAndDropEffectsEnum ToDaliDragAndDropEffectsEnum(DragDropEffects dragDropEffects)
+        {
+            int value = (int)dragDropEffects;
+
+            if (value < 0)
+                return DragAndDropEffectsEnum.Copy;
+
+            if (Enum.TryParse(value.ToString(), out DragAndDropEffectsEnum result))
+                return result;
+
+            throw new ArgumentException("Cannot cast value");
+        }
+
+        private IDictionary<string, object> ToDragDataDictionary(IDataObject dataObject)
+        {
+            Dictionary<string, object> dataDictionary = new Dictionary<string, object>();
+
+            foreach(string dataType in dataObject.GetFormats())
+                dataDictionary.Add(dataType, dataObject.GetData(dataType));
+
+            return dataDictionary;
+        }
+
+        private DragAndDropKeyStatesEnum ToDaliDragAndDropKeyStatesEnum(DragDropKeyStates keyStates)
+        {
+            if (Enum.TryParse(keyStates.ToString(), out DragAndDropKeyStatesEnum daliKeyStates))
+                return daliKeyStates;
+
+            throw new ArgumentException("Cannot cast value");
+        }
+
+        private DragDropEffects ToWpfDragDropEffects(DragAndDropEffectsEnum effectsEnum)
+        {
+            if (Enum.TryParse(effectsEnum.ToString(), out DragDropEffects wpfKeyStates))
+                return wpfKeyStates;
+
+            throw new ArgumentException("Cannot cast value");
+        }
+    }
+}
---
+++ src/Dali/RedSharp.Dali.Common/Events/DragAndDropEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using RedSharp.Dali.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedSharp.Dali.Common.Events
+{
+    public class DragAndDropEventArgs
+    {
+        public DragAndDropEffectsEnum AllowedEffects { get; set; }
+        public IDictionary<string, object> Data { get; set; }
+        public DragAndDropEffectsEnum Effects { get; set; }
+        public DragAndDropKeyStatesEnum KeyStates { get; set; }
+        public bool Handled { get; set; }
+    }
+}

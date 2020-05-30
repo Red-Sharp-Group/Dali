a15f34e60511f2ea3e95f4a4811990c27b4e7190
Влад Стовманенко <flaviusglamfenix@gmail.com>
2020-05-30 19:56:53 +0300

    Implementing class to wrap Dispatcher.

---
+++ src/Dali/Dali/Services/DispatcherService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Threading;
+
+using RedSharp.Dali.Common.Enums;
+using RedSharp.Dali.Common.Interfaces.Services;
+
+namespace RedSharp.Dali.View.Services
+{
+    /// <summary>
+    /// Wraps <see cref="Dispatcher"/> for operation dispatching.
+    /// </summary>
+    class DispatcherService : IDispatcher
+    {
+        /// <summary>
+        /// Object that actually do the work.
+        /// </summary>
+        private readonly Dispatcher _nativeDispatcher;
+
+        /// <summary>
+        /// Mappings between framework independent and WPF dispatcher priority.
+        /// </summary>
+        private IReadOnlyDictionary<DaliDispatcherPriorityEnum, DispatcherPriority> _enumMapper =
+            new ReadOnlyDictionary<DaliDispatcherPriorityEnum, DispatcherPriority>(
+                new Dictionary<DaliDispatcherPriorityEnum, DispatcherPriority>
+                {
+                    { DaliDispatcherPriorityEnum.Canceled, DispatcherPriority.Inactive },
+                    { DaliDispatcherPriorityEnum.Idle, DispatcherPriority.ContextIdle },
+                    { DaliDispatcherPriorityEnum.Background, DispatcherPriority.Background },
+                    { DaliDispatcherPriorityEnum.Render, DispatcherPriority.Render },
+                    { DaliDispatcherPriorityEnum.Foreground, DispatcherPriority.Normal }
+                });
+
+        /// <summary>
+        /// Constructs the <see cref="DialogService"/> instanse.
+        /// </summary>
+        /// <param name="dispatcher">WPF dispatcher.</param>
+        public DispatcherService(Dispatcher dispatcher)
+        {
+            _nativeDispatcher = dispatcher;
+        }
+
+        ///<inheritdoc/>
+        public void BeginInvoke(Action action, DaliDispatcherPriorityEnum priority)
+        {
+            if (action == null)
+                throw new ArgumentNullException($"{nameof(action)} parameter is null");
+
+            _nativeDispatcher.BeginInvoke(action, _enumMapper[priority]);
+        }
+
+        ///<inheritdoc/>
+        public void Invoke(Action action)
+        {
+            if (action == null)
+                throw new ArgumentNullException($"{nameof(action)} parameter is null");
+
+            _nativeDispatcher.Invoke(action);
+        }
+    }
+}
---
+++ src/Dali/RedSharp.Dali.Common/Enums/DaliDispatcherPriorityEnum.cs
@@ -0,0 +1,33 @@
+﻿namespace RedSharp.Dali.Common.Enums
+{
+    /// <summary>
+    /// Defines priority of action for executions.
+    /// </summary>
+    public enum DaliDispatcherPriorityEnum
+    {
+        /// <summary>
+        /// Action will not be executed.
+        /// </summary>
+        Canceled,
+
+        /// <summary>
+        /// Action will be executed after all other background action.
+        /// </summary>
+        Idle,
+
+        /// <summary>
+        /// Action will be executed in background.
+        /// </summary>
+        Background,
+
+        /// <summary>
+        /// Action will be executed in the priority with rendering.
+        /// </summary>
+        Render,
+
+        /// <summary>
+        /// Action will be executed as soon as will be opportunity.
+        /// </summary>
+        Foreground
+    }
+}
